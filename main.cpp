ab4131945de6a652dab2a6bcc4c6cc2632c066dc
William Wennerström <william@willeponken.me>
2018-04-11 18:56:32 +0200

    main: keep it simple stupid, changing URL and resolution with a patch instead.

--- main.cpp
+++ main.cpp
@@ -1,44 +1,17 @@
 #include <QApplication>
 #include <QWebEngineView>
-#include <QCommandLineParser>
-#include <QCommandLineOption>
 
 int main(int argc, char *argv[])
 {
-    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    const QUrl url = QUrl::fromUserInput(QString("https://example.org"));
+    const QSize resolution = QSize(1920, 1080);
 
     QApplication app(argc, argv);
-
-    QCommandLineParser parser;
-    parser.setApplicationDescription("Just a simple webview");
-    parser.addHelpOption();
-    parser.addVersionOption();
-
-    const QCommandLineOption urlOption("u", "URL to show.", "url");
-    const QCommandLineOption resOption("r", "Resolution (e.g. 1920x1080).", "res");
-    parser.addOption(urlOption);
-    parser.addOption(resOption);
-
-    if (!parser.parse(QApplication::arguments())) {
-        qFatal("Invalid command line option.");
-        return 1;
-    }
-
-    QUrl url = QUrl("https://example.org");
-    if (parser.isSet(urlOption)) {
-        url = QUrl::fromUserInput(parser.value(urlOption));
-    }
-
-    QSize res = QSize(1920, 1080);
-    if (parser.isSet(resOption)) {
-        QString resPair = parser.value(resOption);
-        QStringList resList = resPair.split("x");
-        res = QSize(resList[0].toInt(), resList[1].toInt());
-    }
+    app.setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QWebEngineView view;
     view.setUrl(url);
-    view.resize(res);
+    view.resize(resolution);
     view.show();
 
     return app.exec();
